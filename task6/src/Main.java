47be7e8189df195d4622be9bc1ca39450a8d3423
Daniel Dmitry <dmitrj77@gmail.com>
2022-05-14 14:26:39 +0300

    created task_6 branch

---
+++ task6/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] car = new String[]{"Scania", "Man"};
+        Saab saab1 = new Saab ("red","Ikarus","AX3456PC",2020, car);
+        saab1.getModelDescription();
+        Saab saab = new Saab("white", "Scania", "AX3456PI", 2019,45);
+        saab.getModelDescription();
+        Man man = new Man("red", "Man", "BI3456KM", 2010, 10.5F);
+        man.getModelDescription();
+
+    }
+}
